7451e03e263b5905157f7f8e5138bddb699e9ba2
jsirugo <jsirugo@yahoo.com>
2024-02-16 12:18:15 +0100

    Smått o gott

---
+++ script.js
@@ -0,0 +1,18 @@
+let inputBar = document.querySelector('#input');
+let itemList = document.querySelector('#ItemList');
+let itemsLeft = document.querySelector('#counter');
+
+let itemArray = [];
+
+const footer = document.getElementById('#footer');
+
+
+function itemCounter(){
+    let todocounter = 0;
+
+}
+function DisplayFooter(){
+    if(itemsLeft < 1){
+        
+    }
+}
