a470df56a7e0785e28d43f9347ece5e66a6d5419
jsirugo <jsirugo@yahoo.com>
2024-02-16 15:16:30 +0100

    scriptintefunka

--- script.js
+++ script.js
@@ -1,19 +1,75 @@
 let inputBar = document.querySelector('#input');
-let itemList = document.querySelector('#ItemList');
+let itemList = document.querySelector('#itemList');
 let itemsLeft = document.querySelector('#counter');
+let footerButtons = document.getElementById('buttonFooter');
 
 let itemArray = [];
 
-const footerButtons = document.getElementById('#ButtonFooter');
 
+function itemCounter() {
+    let todocounter = itemArray.filter(item => !item.completed).length;
 
-function itemCounter(){
-    let todocounter = 0;
-
-}
-function DisplayFooter(){
-
-    if(itemsLeft < 1){
-
+    itemsLeft.textContent = todocounter;
+  
+    if (todocounter > 0) {
+      footerButtons.style.display = 'flex';
+    } else {
+      footerButtons.style.display = 'none';
     }
+  }
+
+  inputBar.addEventListener('keypress', function(event) {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+      addNewItem();
+    }
+  });
+
+function addNewItem() {
+    let newItem = inputBar.value.trim();
+  
+    if (newItem !== '') {
+      itemArray.push({ text: newItem, completed: false });
+      addItemToDOM(newItem); 
+      itemCounter(); 
+      inputBar.value = ''; 
+    }
+  }
+
+
+function addItemToDOM(newItem) {
+  let newListItem = document.createElement('li');
+  let checkbox = document.createElement('input');
+  let label = document.createElement('label');
+  let deleteButton = document.createElement('button');
+
+  
+  checkbox.type = 'checkbox';
+  label.textContent = newItem;
+  deleteButton.textContent = 'X';
+  deleteButton.classList.add('delete-button'); 
+
+ 
+  label.appendChild(checkbox);
+  newListItem.appendChild(label);
+  newListItem.appendChild(deleteButton);
+  itemList.appendChild(newListItem);
+
+  checkbox.addEventListener('change', function() {
+    let labelText = label.textContent.trim(); 
+
+    let itemToUpdate = itemArray.find(item => item.text === labelText);
+
+    itemToUpdate.completed = checkbox.checked;
+
+    itemCounter();
+});
+  
+  deleteButton.addEventListener('click', () => {
+    
+    newListItem.remove();
+    itemCounter(); 
+  });
 }
+ itemCounter(); 
+
